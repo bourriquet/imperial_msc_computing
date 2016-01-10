4c2421b9638b9b543ff29db58567a9564912eb59
Lauren Anders <lc3311@vm-shell1.doc.ic.ac.uk>
2016-01-10 18:55:32 +0000

    mother base

---
+++ prolog/mother/mother_skeleton.pl
@@ -0,0 +1,30 @@
+% child_mother(C, M) means C is a child of mother M.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+child_mother(amy, mary).
+child_mother(arthur, mary).
+child_mother(angel, jane).
+child_mother(anton, rita).
+child_mother(alan, rita).
+child_mother(axel, susan).
+child_mother(ann, tina).
+
+
+% age(C, A) means C is of age A.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+age(amy, 6).
+age(arthur, 15).
+age(angel, 16).
+age(anton, 4).
+age(alan, 8).
+age(axel, 16).
+age(ann, 4).
+
+
+% employed(X) means X is employed.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+employed(susan).
+employed(rita).
+

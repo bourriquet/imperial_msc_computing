4d4d71f750e48bc02d51c28ee37a92f612eb5698
Lauren Anders <lc3311@matrix09.doc.ic.ac.uk>
2016-01-10 14:38:16 +0000

    hogwarts base

---
+++ prolog/hogwarts/hogwarts_skeleton.pl
@@ -0,0 +1,52 @@
+% the students in Hogwarts
+student(hp, 'Harry James Potter', gryffindor).
+student(hg, 'Hermione Jean Granger', gryffindor).
+student(rw, 'Ronald Weasley', gryffindor).
+student(ll, 'Luna Lovegood', ravenclaw).
+student(cc, 'Cho Chang', ravenclaw).
+student(tb, 'Terry Boot', ravenclaw).
+student(ha, 'Hannah Abbott', hufflepuff).
+student(cd, 'Cedric Diggory', hufflepuff).
+student(nt, 'Nymphadora Tonks',hufflepuff).
+student(dm, 'Draco Malfoy', slytherin).
+student(gg, 'Gregory Goyle', slytherin).
+student(vc, 'Vincent Crabbe', slytherin).
+
+% the teachers in Hogwarts
+teacher(ad, 'Albus Percival Wulfric Brian Dumbledore').
+teacher(ff, 'Filius Flitwick').
+teacher(rh, 'Rubeus Hagrid').
+teacher(gl, 'Gilderoy Lockhart').
+teacher(rl, 'Remus John Lupin').
+teacher(mm, 'Minerva McGonagall').
+teacher(qq, 'Quirinus Quirrell').
+teacher(ss, 'Severus Snape').
+teacher(ps, 'Pomona Sprout').
+teacher(st, 'Sibyll Patricia Trelawney').
+teacher(mh, 'Madam Hooch').
+teacher(as, 'Aurora Sinistra').
+teacher(cub, 'Cuthbert Binns').
+teacher(bb, 'Bathsheba Babbling').
+teacher(sv, 'Septima Vector').
+teacher(chb, 'Charity Burbage').
+teacher(wt, 'Wilkie Twycross').
+
+% compulsory courses for the MSc in Magic
+compCourse(astro, 'Astronomy', as).
+compCourse(charms, 'Charms', ff).
+compCourse(defence, 'Defence against the Dark Arts', qq).
+compCourse(fly, 'Flying', mh).
+compCourse(herb, 'Herbology', ps).
+compCourse(history, 'History of Magic', cub).
+compCourse(potions, 'Potions', ss).
+compCourse(trans, 'Transfiguration', mm).
+
+% optional courses for the MSc in Magic
+optCourse(runes, 'Study of Ancient Runes', bb).
+optCourse(arith, 'Arithmancy', sv).
+optCourse(muggle, 'Muggle Studies', chb).
+optCourse(creatures, 'Care of Magical Creatures', rh).
+optCourse(div, 'Divination', st).
+optCourse(app, 'Apparition', wt).
+optCourse(choir, 'Frog Choir', ff).
+optCourse(quid, 'Quidditch', mh).

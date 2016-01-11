a9cb9214fd610c5df3a10b4f27655275e50815f5
Lauren Anders <lc3311@vm-shell1.doc.ic.ac.uk>
2016-01-11 23:59:13 +0000

    C++ tut6 done

---
+++ c++/tutorials/tut6/ex1/array.h
@@ -0,0 +1,10 @@
+#ifndef ARRAY
+#define ARRAY
+
+void input_array(int a[],int n);
+void display_array(int a[], int n);
+void copy_array(int a1[],int a2[], int n);
+double average(int a[], int n);
+double std_dev(int a[], int n);
+
+#endif
---
+++ c++/tutorials/tut6/ex1/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include "array.h"
+
+using namespace std;
+
+// functions defined in array.cpp
+
+int main() {
+
+  const int size = 5;
+  int first[size];
+  int second[size];
+
+  input_array(first,size);
+  input_array(second,size);
+
+  display_array(first,size);
+  cout << endl;
+  display_array(second,size);
+  cout << endl;
+
+  copy_array(first,second,3);
+  display_array(first,size);
+  cout << endl;
+
+  cout << std_dev(first,size) << endl;
+  cout << std_dev(second,size) << endl;
+
+  return 0;
+
+}

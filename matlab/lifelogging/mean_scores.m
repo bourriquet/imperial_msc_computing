58d8c5bc9fac5a743c97849d299ba987c3ede201
Lauren Anders <laurenanders92@gmail.com>
2016-01-17 16:43:46 +0000

    Update mean_scores.m

--- matlab/lifelogging/mean_scores.m
+++ matlab/lifelogging/mean_scores.m
@@ -1,9 +1,20 @@
+% part of the solution to Q4
 function [Mean] = mean_scores(users, scores)
     Mean = [];
+    
+    % get a matrix of users and their scores
     A = [users scores];
+    
+    % find the max user ID
     m = max(users);
-    n = length(users);
+    
+    % find the length of M
+    n = length(M);
+    
+    % put the user IDs in the matrix to be returned
     Mean(:,1) = [0:m];
+    
+    % loop through every user ID and compute their mean score
     for i = 0:m
         total = 0;
         days = 0;

a86b13d4a8fb97001f6df1e53b6305b5f65cd7aa
Lauren Anders <laurenanders92@gmail.com>
2016-03-19 18:06:36 +0000

    Update OneBuf.java

--- concurrency/tut5/OneBuf.java
+++ concurrency/tut5/OneBuf.java
@@ -1,3 +1,30 @@
 class OneBuf {
 
+  Object slot = null;
+  
+  public synchronized void put(Object o) {
+    while (slot != null) {
+      try {
+        wait();
+      }
+      catch (InterruptedException e) {
+      }
+      slot = o;
+      notify();
+    }
+  }
+  
+  public synchronized void get() {
+    while (slot == null) {
+      try {
+        wait();
+      }
+      catch (InterruptedException e) {
+      }
+      Object o = slot;
+      slot = null;
+      notify();
+      return o;
+    }
+  }
 }

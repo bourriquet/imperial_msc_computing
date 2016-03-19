a44316e19a264fc713ba0a12bf65ebc669a21120
Lauren Anders <laurenanders92@gmail.com>
2016-03-19 18:24:30 +0000

    Update OneBuf2.java

--- concurrency/tut5/OneBuf2.java
+++ concurrency/tut5/OneBuf2.java
@@ -1,19 +1,25 @@
-class OneBuf {
+/*
+Tutorial 5, Q 5.2
+*/
 
-  Object slot = null;
+class OneBuf2 {
+
+  Semaphore full;             // counts number of objects in the buffer
+  Semaphore empty;            // counts number of empty slots in the buffer
+  
+  // constructor
+  OneBuf2() {
+    Object slot = null;
+    full = new Semaphore(0);
+    empty = new Semaphore(1);
+  }
   
   // put an object into the bounded buffer
   public synchronized void put(Object o) {
     // only put object in if the buffer is empty
-    while (slot != null) {
-      try {
-        wait();
-      }
-      catch (InterruptedException e) {
-      }
-      slot = o;
-      notify();
-    }
+    empty.down();
+    slot = o;
+    full.up();
   }
   
   // get an object from the bounded buffer
@@ -25,10 +31,11 @@ class OneBuf {
       }
       catch (InterruptedException e) {
       }
-      Object o = slot;
-      slot = null;
-      notify();
-      return o;
     }
+    full.down();
+    Object o = slot;
+    slot = null;
+    empty.up();
+    return o;
   }
 }

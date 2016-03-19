98d5f0b4157e8dde40caafa91c370566bf1b2485
Lauren Anders <laurenanders92@gmail.com>
2016-03-19 18:32:18 +0000

    Update Barrier.java

--- concurrency/tut5/Barrier.java
+++ concurrency/tut5/Barrier.java
@@ -2,3 +2,27 @@
 Tutorial 5, Q 5.4
 */
 
+class Barrier {
+  
+  int n;
+  int blocked = 0;
+  
+  // constructor
+  Barrier(int n) {
+    this.n = n;
+  }
+  
+  public synchronized void sync() throws InterruptedException{
+    
+    ++blocked;
+    
+    // can only proceed if n processes have called sync()
+    if (blocked < n) {
+      wait();
+    }
+    else {
+      notifyAll();
+      blocked = 0;
+    }
+  }
+}

576aa3c8fb36edb5579ca8e7ead95da05adee04d
Lauren Anders <laurenanders92@gmail.com>
2016-03-19 18:19:11 +0000

    Create OneBuf2.java

---
+++ concurrency/tut5/OneBuf2.java
@@ -0,0 +1,34 @@
+class OneBuf {
+
+  Object slot = null;
+  
+  // put an object into the bounded buffer
+  public synchronized void put(Object o) {
+    // only put object in if the buffer is empty
+    while (slot != null) {
+      try {
+        wait();
+      }
+      catch (InterruptedException e) {
+      }
+      slot = o;
+      notify();
+    }
+  }
+  
+  // get an object from the bounded buffer
+  public synchronized void get() {
+    // only get an object if there buffer is not empty
+    while (slot == null) {
+      try {
+        wait();
+      }
+      catch (InterruptedException e) {
+      }
+      Object o = slot;
+      slot = null;
+      notify();
+      return o;
+    }
+  }
+}

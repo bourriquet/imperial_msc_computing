a9e5fbb2cd3c5a4416a58de52f577e0f1e849565
Lauren Anders <laurenanders92@gmail.com>
2016-04-08 15:15:53 +0100

    Create output.cpp

---
+++ cplusplus/tutorials/tut4/q1/output.cpp
@@ -0,0 +1,35 @@
+/*
+Tutorial 4, Q 1
+
+Implementation file
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+  char character;
+
+  ifstream in;
+
+  // open this file
+  in.open("output.cpp");
+  in.get(character);
+
+  // while the file hasn't ended...
+  while (!in.eof()) {
+    cout << character;
+    in.get(character);
+  }
+
+  // close the file
+  in.close();
+
+  cout << endl;
+
+  return 0;
+
+}

b525887ccdc678196df29c960d582812365814ad
Lauren Anders <laurenanders92@gmail.com>
2016-04-08 17:18:04 +0100

    Create sudoku_skeleton.cpp

---
+++ cplusplus/sudoku/sudoku_skeleton.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+#include "sudoku.h"
+
+using namespace std;
+
+/* You are pre-supplied with the functions below. Add your own 
+   function definitions to the end of this file. */
+
+/* pre-supplied function to load a Sudoku board from a file */
+void load_board(const char *filename, char board[9][9]) {
+
+  cout << "Loading Sudoku board from file '" << filename << "'... ";
+
+  ifstream in(filename);
+  if (!in)
+    cout << "Failed!" << endl;
+  assert(in);
+
+  char buffer[512];
+
+  int row = 0;
+  in.getline(buffer,512);
+  while (in && row < 9) {
+    for (int n=0; n<9; n++) {
+      assert(buffer[n] == '.' || isdigit(buffer[n]));
+      board[row][n] = buffer[n];
+    }
+    row++;
+    in.getline(buffer,512);
+  }
+
+  cout << ((row == 9) ? "Success!" : "Failed!") << endl;
+  assert(row == 9);
+}
+
+/* internal helper function */
+void print_frame(int row) {
+  if (!(row % 3))
+    cout << "  +===========+===========+===========+" << endl;
+  else
+    cout << "  +---+---+---+---+---+---+---+---+---+" << endl;
+}
+
+/* internal helper function */
+void print_row(const char *data, int row) {
+  cout << (char) ('A' + row) << " ";
+  for (int i=0; i<9; i++) {
+    cout << ( (i % 3) ? ':' : '|' ) << " ";
+    cout << ( (data[i]=='.') ? ' ' : data[i]) << " ";
+  }
+  cout << "|" << endl;
+}
+
+/* pre-supplied function to display a Sudoku board */
+void display_board(const char board[9][9]) {
+  cout << "    ";
+  for (int r=0; r<9; r++) 
+    cout << (char) ('1'+r) << "   ";
+  cout << endl;
+  for (int r=0; r<9; r++) {
+    print_frame(r);
+    print_row(board[r],r);
+  }
+  print_frame(9);
+}
+
+/* add your functions here */

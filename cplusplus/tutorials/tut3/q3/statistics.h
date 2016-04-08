5998365341e2503edbf219ea38a4c6ef3f58fb0f
Lauren Anders <laurenanders92@gmail.com>
2016-04-08 15:04:11 +0100

    Create statistics.h

---
+++ cplusplus/tutorials/tut3/q3/statistics.h
@@ -0,0 +1,14 @@
+#ifndef STATISTICS_H
+#define STATISTICS_H
+
+double average(double num1);
+double average(double num1, double num2);
+double average(double num1, double num2, double num3);
+double average(double num1, double num2, double num3, double num4);
+
+double standard_deviation(double num1);
+double standard_deviation(double num1, double num2);
+double standard_deviation(double num1, double num2, double num3);
+double standard_deviation(double num1, double num2, double num3, double num4);
+
+#endif

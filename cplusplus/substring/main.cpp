58a647c42b49515aef57abc0c7fa1afaea103641
Lauren Anders <laurenanders92@gmail.com>
2016-04-09 16:52:23 +0100

    Create main.cpp

---
+++ cplusplus/substring/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "substring.h"
+
+using namespace std;
+
+int main() {
+  cout << substring_position("this", "this is a simple exercise") << endl;
+  cout << substring_position("is", "this is a simple exercise") << endl;
+  cout << substring_position("is a", "this is a simple exercise") << endl;
+  cout << substring_position("is an", "this is a simple exercise") << endl;
+  cout << substring_position("exercise", "this is a simple exercise") << endl;
+  cout << substring_position("simple exercise", "this is a simple") << endl;
+  cout << substring_position("", "this is a simple exercise") << endl;
+  cout << substring_position("", "") << endl;
+
+  return 0;
+}

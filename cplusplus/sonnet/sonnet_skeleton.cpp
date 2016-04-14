43501705452419c549a06ccfef726757b392ec7b
Lauren Anders <laurenanders92@gmail.com>
2016-04-14 17:37:33 +0100

    Create sonnet_skeleton.cpp

---
+++ cplusplus/sonnet/sonnet_skeleton.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <map>
+#include <string>
+
+using namespace std;
+
+#include "sonnet.h"
+
+/* PRE-SUPPLIED HELPER FUNCTIONS START HERE */
+
+/* NOTE: It is much more important to understand the interface to and
+   the "black-box" operation of these functions (in terms of inputs and
+   outputs) than it is to understand the details of their inner working. */
+
+/* get_word(...) retrieves a word from the input string input_line
+   based on its word number. If the word number is valid, the function
+   places an uppercase version of the word in the output parameter
+   output_word, and the function returns true. Otherwise the function
+   returns false. */
+
+bool get_word(const char *input_line, int word_number, char *output_word) {
+  char *output_start = output_word;
+  int words = 0;
+
+  if (word_number < 1) {
+    *output_word = '\0';
+    return false;
+  }
+  
+  do {
+    while (*input_line && !isalnum(*input_line))
+      input_line++;
+
+    if (*input_line == '\0')
+      break;
+
+    output_word = output_start;
+    while (*input_line && (isalnum(*input_line) || *input_line=='\'')) {
+      *output_word = toupper(*input_line);
+      output_word++;
+      input_line++;
+    }
+    *output_word = '\0';
+
+    if (++words == word_number)
+      return true;
+
+  } while (*input_line);
+
+  *output_start = '\0';
+  return false;
+}
+
+/* char rhyming_letter(const char *ending) generates the rhyme scheme
+   letter (starting with 'a') that corresponds to a given line ending
+   (specified as the parameter). The function remembers its state
+   between calls using an internal lookup table, such that subsequents
+   calls with different endings will generate new letters.  The state
+   can be reset (e.g. to start issuing rhyme scheme letters for a new
+   poem) by calling rhyming_letter(RESET). */
+
+char rhyming_letter(const char *ending) {
+
+  // the next rhyming letter to be issued (persists between calls)
+  static char next = 'a';
+  // the table which maps endings to letters (persists between calls)
+  static map<string, char> lookup;
+
+  // providing a way to reset the table between poems
+  if (ending == RESET) {
+    lookup.clear();
+    next = 'a';
+    return '\0';
+  }
+
+  string end(ending);
+
+  // if the ending doesn't exist, add it, and issue a new letter
+  if (lookup.count(end) == 0) {
+    lookup[end]=next;
+    assert(next <= 'z');
+    return next++;
+  }
+
+  // otherwise return the letter corresponding to the existing ending
+  return lookup[end];
+}
+
+/* START WRITING YOUR FUNCTION BODIES HERE */

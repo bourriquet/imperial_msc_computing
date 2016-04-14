6dd90dba231c3b21c01023fba29a9249a2d3f0e3
Lauren Anders <laurenanders92@gmail.com>
2016-04-14 17:02:06 +0100

    Create words.h

---
+++ cplusplus/words/words.h
@@ -0,0 +1,7 @@
+void reverse(const char* str1, char* str2);
+int compare(const char* one, const char* two);
+int palindrome(const char* sentence);
+void remove_rubbish(char* str);
+int anagram(const char* str1, const char* str2);
+int find_smallest(char* str, int start);
+void swap(char& first, char& second);

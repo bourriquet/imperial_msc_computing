8a4e9ee3f6345c23e1ff4757378c79a390f9f976
Lauren Anders <laurenanders92@gmail.com>
2016-04-14 19:16:25 +0100

    Create maze.cpp

---
+++ cplusplus/maze/maze.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+
+using namespace std;
+
+/* You are pre-supplied with the functions below. Add your own 
+   function definitions to the end of this file. */
+
+/* helper function which allocates a dynamic 2D array */
+char **allocate_2D_array(int rows, int columns) {
+  char **m = new char *[rows];
+  assert(m);
+  for (int r=0; r<rows; r++) {
+    m[r] = new char[columns];
+    assert(m[r]);
+  }
+  return m;
+}
+
+/* helper function which deallocates a dynamic 2D array */
+void deallocate_2D_array(char **m, int rows) {
+  for (int r=0; r<rows; r++)
+    delete [] m[r];
+  delete [] m;
+}
+
+/* helper function for internal use only which gets the dimensions of a maze */
+bool get_maze_dimensions(const char *filename, int &height, int &width) {
+  char line[512];
+  
+  ifstream input(filename);
+
+  height = width = 0;
+
+  input.getline(line,512);  
+  while (input) {
+    if ( (int) strlen(line) > width)
+      width = strlen(line);
+    height++;
+    input.getline(line,512);  
+  }
+
+  if (height > 0)
+    return true;
+  return false;
+}
+
+/* pre-supplied function to load a maze from a file*/
+char **load_maze(const char *filename, int &height, int &width) {
+
+  bool success = get_maze_dimensions(filename, height, width);
+  
+  if (!success)
+    return NULL;
+
+  char **m = allocate_2D_array(height, width);
+  
+  ifstream input(filename);
+
+  char line[512];
+
+  for (int r = 0; r<height; r++) {
+    input.getline(line, 512);
+    strcpy(m[r], line);
+  }
+  
+  return m;
+}
+
+/* pre-supplied function to print a maze */
+void print_maze(char **m, int height, int width) {
+  cout << setw(4) << " " << " ";
+  for (int c=0; c<width; c++)
+    if (c && (c % 10) == 0) 
+      cout << c/10;
+    else
+      cout << " ";
+  cout << endl;
+
+  cout << setw(4) << " " << " ";
+  for (int c=0; c<width; c++)
+    cout << (c % 10);
+  cout << endl;
+
+  for (int r=0; r<height; r++) {
+    cout << setw(4) << r << " ";    
+    for (int c=0; c<width; c++) 
+      cout << m[r][c];
+    cout << endl;
+  }
+}

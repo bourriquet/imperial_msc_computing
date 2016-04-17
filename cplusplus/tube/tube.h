d0f5df72697389fc7332e512c83f49fcfd6be3e8
Lauren Anders <laurenanders92@gmail.com>
2016-04-17 14:19:52 +0100

    Create tube.h

---
+++ cplusplus/tube/tube.h
@@ -0,0 +1,22 @@
+enum Direction {N, S, W, E, NE, NW, SE, SW, END, INVALID_DIRECTION};
+
+/* error codes for Question 3 */
+#define ERROR_START_STATION_INVALID -1
+#define ERROR_ROUTE_ENDPOINT_IS_NOT_STATION -2
+#define ERROR_LINE_HOPPING_BETWEEN_STATIONS -3
+#define ERROR_BACKTRACKING_BETWEEN_STATIONS -4
+#define ERROR_INVALID_DIRECTION -5
+#define ERROR_OFF_TRACK -6
+#define ERROR_OUT_OF_BOUNDS -7
+
+/* pre-supplied function to load a tube map from a file*/
+char **load_map(const char *filename, int &height, int &width);
+
+/* pre-supplied function to print the tube map */
+void print_map(char **m, int height, int width);
+
+/* pre-supplied helper function to describe errors for Question 3 */
+const char *error_description(int code);
+
+/* presupplied helper function for converting string to Direction enum */
+Direction string_to_direction(const char *token);
